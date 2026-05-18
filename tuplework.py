8c595fe3de586f7b73735ac09463ab3e4b00aaf1
itsmesangam <sangamspk027@gmail.com>
2026-05-18 14:02:53 +0545

    this is tuple work

---
+++ tuplework.py
@@ -0,0 +1,13 @@
+#name = ("sangam", "sajok", "aadarshan", "Karuna", "Rajesh", "Suresh")
+#print(name[0])
+#home_address = (123.22, 133.44)
+#print(home_address[-1])
+#print Jan to Dec month name using tuple and print month name useing loop or without using loop
+month = ("janaury", "Februray", "March", "April", "May", "June", "July", "August",
+ "September", "October", "November", "December")
+ #using loop
+for x in month:
+        print(x)
+        
+
+
