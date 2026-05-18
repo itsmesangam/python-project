114e95bcc7c144333588abbdd590018544295a5b
itsmesangam <sangamspk027@gmail.com>
2026-05-18 14:42:39 +0545

    combined tuple

--- tuplework.py
+++ tuplework.py
@@ -3,11 +3,17 @@
 #home_address = (123.22, 133.44)
 #print(home_address[-1])
 #print Jan to Dec month name using tuple and print month name useing loop or without using loop
-month = ("janaury", "Februray", "March", "April", "May", "June", "July", "August",
- "September", "October", "November", "December")
- #using loop
-for x in month:
-        print(x)
+# month = ("janaury", "Februray", "March", "April", "May", "June", "July", "August",
+#  "September", "October", "November", "December")
+#  using loop
+# for x in month:
+#        print(x)
         
+vegetables = ("potato", "tomato", "onion", "garlic", "carrot")
+fruits = ("apple", "banana", "cherry", "orange", "pineapple")
+favourite_food = vegetables + fruits
+print(f"combined tuple : {favourite_food}")
+print(len(favourite_food))
+
 
 
