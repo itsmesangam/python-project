c24a4593991e58aba236eec0f38472add4f6cdc6
itsmesangam <sangamspk027@gmail.com>
2026-05-24 16:37:54 +0545

    contactdic addded to the file.

---
+++ contactdic.py
@@ -0,0 +1,17 @@
+contact_list = {
+  "name" : "sangam Sapkota",
+  "phone" : "9845774937",
+  "gmail" : "sangamspk027@gmail.com",
+  "address" : {
+        "temporary" : "Bharatpur-10",
+        "Permanent" : "Bharatpur-25"
+  }
+  }
+    
+print(contact_list["name"])
+print(contact_list["phone"])
+print(contact_list["gmail"])
+print(contact_list["address"]["Permanent"])
+
+
+
