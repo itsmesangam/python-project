36fdb0934d174896ca68a631a034e49b32191624
itsmesangam <sangamspk027@gmail.com>
2026-07-14 15:23:59 +0545

    this is the function

---
+++ function.py
@@ -0,0 +1,13 @@
+#creat the function and print the name
+# def display_name(name):
+#   print(f"my name is {name}")
+# display_name("sangam Sapkota")
+#create the function that print square number
+def square_num(num):
+  num = int(input("enter the number : "))
+  square_num = num ** 2 
+  print(f"square of {num} is {square_num} ")
+square_num(square_num)
+
+  
+  
